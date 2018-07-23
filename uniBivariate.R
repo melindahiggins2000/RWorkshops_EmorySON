89d47824d8905de8695e117479999c7b73031c82
Melinda Higgins <melinda.higgins@emory.edu>
2018-07-23 11:36:45 -0400

    update workshop 2 code

--- uniBivariate.R
+++ uniBivariate.R
@@ -1,8 +1,9 @@
 #' =======================================
-#' Working with the HELP dataset
+#' Univariate and Bivariate Stats and
+#' Graphics for Visualizing data
 #' 
 #' Melinda Higgins, PhD
-#' dated 09/17/2017
+#' dated 07/23/2018
 #' =======================================
 
 # The *.Rdata file can be downloaded from the SASR website
@@ -36,28 +37,63 @@ ncol(helpdata)
 # for a data.frame. However, you can use length()
 # to get the number of elements in a given column.
 # say the pcs1 variable.
+
+# get the number of variables, number of columns
 length(helpdata)
+
+# get the number of rows, observations
 length(helpdata$pcs1)
+
+# simply summary stats
+summary(helpdata$pcs1)
+
+# notice that this doesn't give us the
+# standard deviation
+# other stats specific functions
+mean(helpdata$pcs1)
+sd(helpdata$pcs1)
+
+# why did we get NA's - look back at the summary stats
+# output - notice there were 207 NA's or missing values
+# we have to add an option to handle the missing data
+mean(helpdata$pcs1, na.rm = TRUE)
+sd(helpdata$pcs1, na.rm = TRUE)
+
+# to see amount of missing data in
+# a given variable - try pcs1
+summary(helpdata$pcs1)
+
+# is.na() returns a logic statement
+# of TRUE/FALSE which can be summed 
+# to count the number of TRUE's as 1's
+sum(is.na(helpdata$pcs1))
+
+# can also use mean() to find the percentage
+# of missing TRUE's - there are 207 missing
+# out of 453 = 0.45695 (45.7% missing)
+mean(is.na(helpdata$pcs1))
+
+# suppose we wanted the 2.5th and 97.5th
+# percentiles to find the middle 95% of the data
+quantile(helpdata$pcs1, probs = c(0.025, 0.975),
+         na.rm = TRUE)
+
+# more ways to summarize data
 
 # using "tidyverse" approach with pipes %>%
 # load the dplyr package
 library(dplyr)
 
-# run a basic summary of
-# age, pcs, mcs and cesd
-hsub <- helpdata %>%
-  select(age,pcs,mcs,cesd) %>%
+# run summary stats for pcs1
+helpdata %>%
+  select(pcs1) %>%
   summary()
 
-# this summary object is a "table"
-class(hsub)
-hsub
-
-# table objects can be printed using the 
-# kable() function in the knitr package
-# use inside of rmarkdown file to make nice tables
-# using rmarkdown
-knitr::kable(hsub)
+# run a basic summary of
+# age, pcs, mcs and cesd
+helpdata %>%
+  select(age,pcs,mcs,cesd) %>%
+  summary()
 
 # try the Hmisc package - use describe() function
 library(Hmisc)
@@ -66,33 +102,24 @@ library(Hmisc)
 # at a time
 Hmisc::describe(helpdata$age)
 Hmisc::describe(helpdata$racegrp)
+Hmisc::describe(helpdata$pcs)
+Hmisc::describe(helpdata$pcs1)
 
 # get descriptive stats for a selection of vars
 helpdata %>%
   select(age,pcs,mcs,cesd) %>%
   Hmisc::describe()
 
-hmiscout <- helpdata %>%
-  select(age,female,substance,racegrp) %>%
-  Hmisc::describe()
-
-hmiscout
-
 # try pastecs package - stat.desc() function
 library(pastecs)
 
-pastecsout <- helpdata %>%
-  select(age,pcs,mcs,cesd) %>%
+helpdata %>%
+  select(pcs1) %>%
   pastecs::stat.desc()
 
-pastecsout
-
-# this creates a data.frame
-# which can also be formatted into a table
-# using knitr::kable() in rmarkdown
-class(pastecsout)
-
-knitr::kable(pastecsout)
+helpdata %>%
+  select(age,pcs,mcs,cesd) %>%
+  pastecs::stat.desc()
 
 # also try the psych package
 # describe() function
@@ -103,47 +130,11 @@ knitr::kable(pastecsout)
 
 library(psych)
 
-psychout <- helpdata %>%
+helpdata %>%
   select(age,pcs,mcs,cesd) %>%
   psych::describe()
 
-# this output is also a data.frame
-class(psychout)
-
-knitr::kable(psychout)
-
-# ==========================================
-# OPTIONAL
-# descriptives with N741 package 
-#     - by Melinda Higgins
-# ==========================================
-
-devtools::install_github("melindahiggins2000/N741pkg", 
-                         build_vignettes=TRUE, 
-                         force=TRUE)
-library(N741pkg)
-# if not already loaded, you also need
-# library(dplyr)
-
-h <- helpdata
-t1 <- N741pkg::tbl.continuous(h,h$age,"Age")
-t2 <- N741pkg::tbl.continuous(h,h$pcs,"SF12 Physical Component Score")
-t3 <- N741pkg::tbl.continuous(h,h$mcs,"SF12 Mental Component Score")
-t4 <- N741pkg::tbl.continuous(h,h$cesd,"CESD Depressive Symptoms")
-knitr::kable(rbind(t1,t2,t3,t4),
-             caption = "Table of Summary Stats for Numeric Variables in `helpdata`")
-
-gm <- dplyr::group_by(h,substance)
-t1 <- N741pkg::tbl.cat(gm,gm$substance)
-knitr::kable(t1,
-             caption = "Frequency Table for Substance Use")
-
-gm <- dplyr::group_by(h,racegrp)
-t1 <- N741pkg::tbl.cat(gm,gm$racegrp)
-knitr::kable(t1,
-             caption = "Frequency Table for Racial Groups")
-
-# other ways to get frequency tables
+# ways to get frequency tables
 library(gmodels)
 gmodels::CrossTable(helpdata$racegrp) # SAS format default
 gmodels::CrossTable(helpdata$racegrp, format="SPSS")
@@ -197,7 +188,14 @@ boxplot(helpdata$age)
 # simple boxplot - get stats
 b1 <- boxplot(helpdata$age)
 b1$stats
+# these 5 numbers are the lower whisker value,
+# lower hinge (25th percentile), median,
+# upper hinge (75th percentile), upper whisker
 
+# compare to summary and fivenum
+summary(helpdata$age)
+
+# another way to get the stats for a boxplot
 boxplot.stats(helpdata$age)
 
 # Tukey's five number summary
@@ -212,11 +210,11 @@ help("fivenum")
 # default, returns min, 25th, median, 75th, max
 quantile(helpdata$age)
 
-# can use different algorithms
-quantile(helpdata$age, type=3)
-quantile(helpdata$age, type=9)
+# change the probs to get other percentiles
+quantile(helpdata$age, probs = c(0.025, 0.975))
 
 # boxplots by groups
+# use formula approach using ~
 boxplot(age ~ racegrp, data=helpdata,
         main="Age Distributions by Race",
         xlab="Race",
@@ -235,6 +233,7 @@ ggplot(helpdata, aes(x=racegrp, y=age, fill=racegrp)) +
   xlab("Racial Group") +
   ylab("Age (in years)")
 
+# an alternative to boxes
 # ages by race - violin plots
 ggplot(helpdata, aes(x=racegrp, y=age, fill=racegrp)) + 
   geom_violin() +
@@ -254,59 +253,14 @@ ggplot(helpdata, aes(x=racegrp, y=age, fill=racegrp)) +
 
 # optional
 # violin plots - by race
+# using the violinplot package
 library(violinmplot)
 violinmplot(age ~ racegrp, 
-            data=helpdata)
+            data = helpdata)
 
 # categorical data - frequency plots
 helpdata %>% 
   ggplot(aes(x=racegrp)) +
     geom_bar(stat="count")
 
-# to see amount of missing data in
-# a given variable - try pcs1
-summary(helpdata$pcs1)
 
-# is.na() returns a logic statement
-# of TRUE/FALSE which can be summed 
-# to count the number of TRUE's as 1's
-sum(is.na(helpdata$pcs1))
-
-# can also use mean() to find the percentage
-# of missing TRUE's - there are 207 missing
-# out of 453 = 0.45695 (45.7% missing)
-mean(is.na(helpdata$pcs1))
-
-# optional - the purrr package
-# can be used to map the mean() function
-# across every column to get the 
-# amount of missing data for every column/var
-# in the dataset
-# 
-# learn more about purrr at
-# http://purrr.tidyverse.org/ and 
-# from Jenny Bryan's tutorial 
-# https://jennybc.github.io/purrr-tutorial/ 
-library(purrr)
-outmiss <- helpdata %>% map(~ mean(is.na(.)))
-
-# This is a list output - convert to data.frame
-# take the transpose to be 88-x-1 instead of 1-x-88
-# use knitr::kable() to print table in rmarkdown
-odf <- t(as.data.frame(outmiss))
-
-# multiply by 100 to get %
-knitr::kable(odf*100,
-             col.names = "% Missing")
-
-# another example - cesd across time
-# cesd, cesd1, cesd2, cesd3, cesd4
-# using the N741pkg package and tbl.continuous() function
-h <- helpdata
-t1 <- N741pkg::tbl.continuous(h,h$cesd,"CESD - Baseline")
-t2 <- N741pkg::tbl.continuous(h,h$cesd1,"CESD - 6m")
-t3 <- N741pkg::tbl.continuous(h,h$cesd2,"CESD - 12m")
-t4 <- N741pkg::tbl.continuous(h,h$cesd3,"CESD - 18m")
-t5 <- N741pkg::tbl.continuous(h,h$cesd3,"CESD - 24m")
-knitr::kable(rbind(t1,t2,t3,t4,t5),
-             caption = "Table of Summary Stats for CESD - 5 time points")
